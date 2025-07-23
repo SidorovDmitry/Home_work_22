8ccc9b3ff030b50ea7c5d5cdecb8a57f314b4917
Dmitri_Sidorov <kin19861@rambler.ru>
2025-07-23 21:50:02 +0300

    comp

--- config/settings.py
+++ config/settings.py
@@ -39,12 +39,12 @@ ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.templates.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.request',
+                'django.templates.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
