4a5bea056b1185a0a8fb0cf6db937a89cdf7b7d2
Dmitri_Sidorov <kin19861@rambler.ru>
2025-08-03 21:57:00 +0300

    correct path/catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from . import views
+from catalog.views import home, contacts, product_card, product_catalog, pay
 
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path("product_card/<int:product_id>/", product_card, name="product_card"),
+    path('product_catalog/', product_catalog, name='product_catalog'),
+    path('pay/', pay, name='pay'),
 ]
