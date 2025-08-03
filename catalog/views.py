9504a75693bb447f9fbab3125cfb379214e888aa
Dmitri_Sidorov <kin19861@rambler.ru>
2025-08-03 21:55:50 +0300

    correct README.md

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Product
 
 def home(request):
     return render(request, template_name="home.html")
@@ -14,3 +14,18 @@ def contacts(request):
         message = request.POST.get('message')
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'contacts.html')
+
+
+def product_card(request,product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    context = {"product": product}
+    return render(request, "product_card.html", context=context)
+
+
+def product_catalog(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'product_catalog.html', context=context)
+
+def pay(request):
+    return render(request, "pay.html")
