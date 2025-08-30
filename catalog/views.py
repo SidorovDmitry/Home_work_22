940e907c45fb0195b30d768c750f9a685f8ec269
Dmitri_Sidorov <kin19861@rambler.ru>
2025-08-30 18:58:05 +0300

    add and correct views/catalog

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,31 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, TemplateView, DetailView, DeleteView
 from django.views.generic.edit import CreateView, UpdateView
 from catalog.models import Product
 from catalog.forms import ProductForm
+from django.views import View
+from django.shortcuts import get_object_or_404, redirect
+
+
+class ProductPublishView(PermissionRequiredMixin, View):
+    permission_required = 'catalog.can_publish_product'
+
+    def post(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.is_published = True
+        product.save()
+        return redirect('catalog:product_detail', pk=pk)
+
+
+class ProductUnpublishView(PermissionRequiredMixin, View):
+    permission_required = 'catalog.can_unpublish_product'
+
+    def post(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.is_published = False
+        product.save()
+        return redirect('catalog:product_detail', pk=pk)
 
 class HomeView(TemplateView):
     template_name = 'home.html'
@@ -25,11 +47,22 @@ class ProductListView(ListView):
     template_name = 'product_catalog.html'
     context_object_name = "products"
 
+    def get_queryset(self):
+        if self.request.user.has_perm('catalog.can_publish_product'):
+            return Product.objects.all()
+        return Product.objects.filter(is_published=True)
+
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'product_card.html'
     context_object_name = 'product'
+
+    def get_queryset(self):
+        if self.request.user.has_perm('catalog.can_publish_product'):
+            return Product.objects.all()
+        return Product.objects.filter(is_published=True)
+
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
@@ -37,16 +70,31 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     template_name = 'product_form.html'
     success_url = reverse_lazy('catalog:product_catalog')
 
-class ProductUpdateView(LoginRequiredMixin,UpdateView):
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Product
     form_class = ProductForm
 
+    def test_func(self):
+        product = self.get_object()
+        return product.owner == self.request.user
+
     def get_success_url(self):
         """ Перенаправление на страницу созданного блога. """
-        return reverse("catalog:product_card",  kwargs={'pk': self.object.pk})
+        return reverse("catalog:product_card", kwargs={'pk': self.object.pk})
 
-class ProductDeleteView(LoginRequiredMixin,PermissionRequiredMixin, DeleteView):
+
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:product_catalog')
-    permission_required = 'catalog.delete_products'
     raise_exception = True
+
+    def test_func(self):
+        product = self.get_object()
+        # Может удалять владелец или модератор
+        return (product.owner == self.request.user or
+                self.request.user.has_perm('catalog.delete_product'))
