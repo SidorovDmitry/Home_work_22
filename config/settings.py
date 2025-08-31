867eaaacd080503a37739ae56b7031f582ebfbfa
Dmitri_Sidorov <kin19861@rambler.ru>
2025-08-31 22:07:41 +0300

    add path and config CACHEs

--- config/settings.py
+++ config/settings.py
@@ -151,3 +151,13 @@ EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
+CHAHE_ENABLE = True
+if CHAHE_ENABLE:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://127.0.0.1:6379/1',
+        }
+    }
